3d4e8403387a7a6573df24ebde6a518d57ccbe45
Samrath Singh <148793870+Samrath18@users.noreply.github.com>
2025-07-30 12:38:07 -0400

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react"
+
+export default function Main() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+    const [memeList, setMemeList] = useState([])
+
+    useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setMemeList(data.data.memes))
+    }, [])
+
+    function handleChange(event) {
+        const { value, name } = event.currentTarget
+        setMeme(prevMeme => {
+            return (
+                { ...prevMeme, [name]: value }
+            )
+        })
+    }
+
+    function newImage() {
+        setMeme({
+            ...meme,
+            imageUrl:  `${memeList[Math.floor(Math.random() * memeList.length)].url}`
+        })
+    }
+
+    return (
+        <main>
+            <div className="form">
+                <label>Top Text
+                    <input
+                        type="text"
+                        placeholder="One does not simply"
+                        name="topText"
+                        onChange={handleChange}
+                        value={meme.topText}
+                    />
+                </label>
+
+                <label>Bottom Text
+                    <input
+                        type="text"
+                        placeholder="Walk into Mordor"
+                        name="bottomText"
+                        onChange={handleChange}
+                        value={meme.bottomText}
+
+                    />
+                </label>
+                <button onClick={newImage}>Get a new meme image 🖼</button>
+            </div>
+            <div className="meme">
+                <img src={meme.imageUrl} />
+                <span className="top">{meme.topText}</span>
+                <span className="bottom">{meme.bottomText}</span>
+            </div>
+        </main>
+    )
+}
